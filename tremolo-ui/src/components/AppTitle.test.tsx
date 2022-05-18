9c364818d8e488af3cd80eaad4c5adeb0a2427b0
Naohito-T <48505736+naohito-T@users.noreply.github.com>
2022-05-18 11:28:08 +0900

    Feature/eslint prettier setup (#11)
    * :white_check_mark: eslint & prettier導入 & workflow導入
    
    * :white_check_mark: workflow修正
    
    * :wrench: vscode config ファイル編集
    
    * :memo: lofthookを導入
    
    * :memo: lefthooks修正

--- tremolo-ui/src/components/AppTitle.test.tsx
+++ tremolo-ui/src/components/AppTitle.test.tsx
@@ -1,11 +1,11 @@
 /**
  * @jest-environment jsdom
  */
-import React from "react";
-import { render } from "@testing-library/react";
-import AppTitle from "./AppTitle";
+import { render } from '@testing-library/react';
+import React from 'react';
+import AppTitle from './AppTitle';
 
-test("AppTitle", () => {
+test('AppTitle', () => {
   const { asFragment } = render(<AppTitle />);
   expect(asFragment()).toMatchSnapshot();
 });
--- tremolo-ui/src/pages/__tests__/index.test.tsx
+++ tremolo-ui/src/pages/__tests__/index.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
-import Home from "@/pages";
+import { render, screen } from '@testing-library/react';
+import Home from '@/pages';
 
-describe("Home", () => {
-  it("renders a heading", () => {
+describe('Home', () => {
+  it('renders a heading', () => {
     render(<Home />);
 
-    const heading = screen.getByRole("heading", {
+    const heading = screen.getByRole('heading', {
       name: /welcome to next\.js!/i,
     });
 
